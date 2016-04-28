b896a35a7d18c34a6697ada61a8860f68b1f577b
Michal Hans <mhans@pgs-soft.com>
2016-04-28 17:30:46 +0200

    Radar directive styling + labels

--- src/app/components/radar/radar.directive.js
+++ src/app/components/radar/radar.directive.js
@@ -22,47 +22,56 @@ export function RadarDirective() {
     scope.counter = items.length;
     angular.forEach(items, function (item) {
       item.position = {};
+
       switch (item.status) {
         case 'Adopt':
-          item.position.y = 10;
-          break;
-        case 'Assess':
-          item.position.y = 20;
+          item.position.r = 1;
           break;
         case 'Trial':
-          item.position.y = 30;
+          item.position.r = 2;
+          break;
+        case 'Assess':
+          item.position.r = 3;
           break;
         case 'Hold':
-          item.position.y = 40;
+          item.position.r = 4;
           break;
         default:
-          item.position.y = 0;
           break;
       }
+
+      // Center radio
+      item.position.r = (item.position.r * 12.5) - 7 ;
+
+      // Randomize radio
+      item.position.r = item.position.r + _.random(-5, 5);
 
       switch (item.area) {
+        case 'Tools':
+          item.position.q = 0;
+          break;
         case 'Techniques':
-          item.position.x = -20;
+          item.position.q = 90;
           break;
         case 'Platforms':
-          item.position.x = 20;
-          break;
-        case 'Tools':
-          item.position.x = -20;
-          item.position.y = -item.position.y
+          item.position.q = 180;
           break;
         case 'Languages & Frameworks':
-          item.position.x = 20;
-          item.position.y = -item.position.y
+          item.position.q = 270;
           break;
         default:
-          item.position.x = 0;
           break;
       }
 
-      item.position.x = item.position.x + _.random(-10, 10);
-      item.position.y = item.position.y + _.random(-10, 10);
-    })
+      // Center angle
+      item.position.q += 45;
 
+      // Randomize angle
+      item.position.q = item.position.q + _.random(-35, 35);
+
+      // Convert polar to cartesian
+      item.position.x =  Math.floor(item.position.r * Math.cos(item.position.q));
+      item.position.y = Math.floor(item.position.r * Math.sin(item.position.q));
+    })
   }
 }
