fe268218b1e9f41eebb2be8206ce20f2af8b4310
Michal Hans <info@michalhans.pl>
2016-07-18 15:30:30 +0200

    Home - hide private radars

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -7,6 +7,8 @@ export class HomePageController {
 
     this.AuthService = AuthService;
 
+    vm.showPrivate = AuthService.currentUser.uid === "4bc0f5c0-1457-4b35-ad63-780c14636610";
+
     // download the data into a local object
     vm.radars = $firebaseArray(itemsRef);
   }
